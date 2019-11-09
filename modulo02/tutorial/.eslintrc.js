61e50854e4bc88896a5e13b861907c4284d4603b
David Maisonnette Jentjens <drex1998@hotmail.com>
2019-11-09 17:44:58 -0300

    Terminando tutorial do modulo02

--- modulo02/tutorial/.eslintrc.js
+++ modulo02/tutorial/.eslintrc.js
@@ -14,6 +14,12 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "function-paren-newline": "off",
+    "implicit-arrow-linebreak": "off",
+    "no-confusing-arrow": "off",
+    "object-curly-newline": "off",
+    "comma-dangle": ["error", "only-multiline"],
+    "arrow-parens": ["error", "as-needed"],
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
---
+++ modulo02/tutorial/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '252b1dcacd154cddcacb92fabe15eb07',
+  expiresIn: '7d',
+};
