d90dd4aa59c6a6c08e2883cebabdd1bed5075adb
David Maisonnette Jentjens <drex1998@hotmail.com>
2019-11-10 17:33:42 -0300

    Completando desafio do modulo02

--- modulo02/desafio/src/database/index.js
+++ modulo02/desafio/src/database/index.js
@@ -2,6 +2,11 @@ import Sequelize from 'sequelize';
 
 import databaseConfig from '../config/database';
 
+import User from '../app/models/User';
+import Student from '../app/models/Student';
+
+const models = [User, Student];
+
 class Database {
   constructor() {
     this.init();
@@ -10,7 +15,7 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    // models.map(model => model.init(this.connection));
+    models.map(model => model.init(this.connection));
   }
 }
 
