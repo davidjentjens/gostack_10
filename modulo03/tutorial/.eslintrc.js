ddbb6c20564ffe47c9c46032ce81944f02447187
David Maisonnette Jentjens <drex1998@hotmail.com>
2020-02-08 16:06:49 -0300

    Update and back on track.

--- modulo03/tutorial/.eslintrc.js
+++ modulo03/tutorial/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
+    node: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
@@ -14,17 +14,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "function-paren-newline": "off",
-    "implicit-arrow-linebreak": "off",
-    "no-confusing-arrow": "off",
-    "object-curly-newline": "off",
-    "comma-dangle": ["error", "only-multiline"],
-    "arrow-parens": ["error", "as-needed"],
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
     "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
-    "linebreak-style": ["error", "windows"]
   },
 };
